f5c56c0153687986ae8c951be40d0fc42358a1c6
Salih Atiç <147946955+ymsalih@users.noreply.github.com>
2025-07-24 14:54:58 +0300

    feat(ViewModel):Görev ve Proje verilerini ortak kullanabilmek için Razor Page bağlandı

---
+++ TaskTracker/Models/ViewModels/UserTaskViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskTracker.Models.ViewModels
+{// bu sınıf razor page için veri taşıyıxı görevi görür 
+    // ef core ile ilişkili değildir 
+    public class UserTaskViewModel
+    {
+        public string TaskTitle { get; set; }
+        public string TaskDetail { get; set; }
+        public DateTime? DueDate { get; set; }
+      
+
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string ProjectFeature { get; set; }
+
+    }
+}
