1e3d25fb62bfed7c1a72e88ebeed3d0e3e6bb0a1
Salih Atiç <147946955+ymsalih@users.noreply.github.com>
2025-07-25 14:20:18 +0300

    feat:Kullanıcı silindiği zaman aktif olarak görevli olduğu işten de silinmesi sağlandı.

---
+++ TaskTracker/Migrations/20250725083716_FixUserDeleteBahaivor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserDeleteBahaivor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedUserId",
+                table: "Tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedUserId",
+                table: "Tasks",
+                column: "AssignedUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_AssignedUserId",
+                table: "Tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_AssignedUserId",
+                table: "Tasks",
+                column: "AssignedUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
