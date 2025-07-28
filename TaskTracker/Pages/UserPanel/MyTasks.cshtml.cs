55d652faba84f6d5667b4bad53b56573a8458db1
Salih Atiç <147946955+ymsalih@users.noreply.github.com>
2025-07-28 12:36:11 +0300

    feat: Yöneticinin yaptığı değişiklik ve görev atama durununda kullanıcının kendine ait tüm görevler gösterildi.

--- TaskTracker/Pages/UserPanel/MyTasks.cshtml.cs
+++ TaskTracker/Pages/UserPanel/MyTasks.cshtml.cs
@@ -2,40 +2,45 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TaskTracker.Infrastructure.Data;
-using TaskTracker.Models.ViewModels; // ViewModel’i burada tanýmladýðýný varsayýyoruz
+using TaskTracker.Models.ViewModels;
 
 public class MyTasksModel : PageModel
 {
     private readonly AppDbContext _context;
-    public List<UserTaskViewModel> MyTasks { get; set; } // artýk farklý yerden verileri viewmodel ile filtrelemiþ halini veriyoruz 
+    public List<UserTaskViewModel> MyTasks { get; set; } = new();
 
     public MyTasksModel(AppDbContext context)
     {
         _context = context;
     }
 
-    public IActionResult OnGet()
+    public async Task<IActionResult> OnGetAsync()
     {
+       
+
         var role = HttpContext.Session.GetString("UserType");
         var userId = HttpContext.Session.GetInt32("UserId");
 
         if (role != "Kullanýcý" || userId == null)
             return RedirectToPage("/AccessDenied");
 
-        MyTasks = _context.Tasks
-            .Include(t => t.Project) // Proje verilerini çekiyoruz içeriðine katýyoruz aslýnda 
-            .Where(t => t.AssignedUserId == userId)
+        MyTasks = await _context.Tasks
+            .Include(t => t.Project)
+            .Include(t => t.TaskUsers)
+            .Where(t =>
+                t.AssignedUserId == userId ||              // Tekil görevler
+                t.TaskUsers.Any(tu => tu.UserId == userId) // Çoklu görevler
+            )
             .Select(t => new UserTaskViewModel
             {
                 TaskTitle = t.Title,
                 TaskDetail = t.Description,
-                DueDate =t.DueDate,  
+                DueDate = t.DueDate,
                 ProjectName = t.Project.Title,
                 ProjectDescription = t.Project.Description,
                 ProjectFeature = t.Project.Feature
-                
             })
-            .ToList();
+            .ToListAsync();
 
         return Page();
     }
