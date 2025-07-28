edc23260fb74999b7afddecccf505e1cfe637fea
Salih Atiç <147946955+ymsalih@users.noreply.github.com>
2025-07-28 15:00:10 +0300

    feat: kullanıcı panelinde görev durumunu bağlamak için TaskStatus eklendi.

--- TaskTracker/Models/ViewModels/UserTaskViewModel.cs
+++ TaskTracker/Models/ViewModels/UserTaskViewModel.cs
@@ -1,22 +1,18 @@
-﻿using Microsoft.VisualBasic;
-
-namespace TaskTracker.Models.ViewModels
-{// bu sınıf razor page için veri taşıyıxı görevi görür 
-    // ef core ile ilişkili değildir 
-    // amaç zaten ef ile almayıp çünkü o direk razora bağlı olmasın diye bu şekilde güncelledik 
+﻿namespace TaskTracker.Models.ViewModels
+{
+    // Bu sınıf Razor Page için veri taşıyıcıdır (EF Core ile doğrudan ilişkili değil)
+    // Görev detaylarını ve proje bilgilerini taşır, ayrıca işlem yapmak için TaskId eklendi
     public class UserTaskViewModel
     {
+       
+
         public string TaskTitle { get; set; }
         public string TaskDetail { get; set; }
         public DateTime? DueDate { get; set; }
-        public string TaskStatus {  get; set; }
-        
-      
-        
+        public string TaskStatus { get; set; }
 
         public string ProjectName { get; set; }
         public string ProjectDescription { get; set; }
         public string ProjectFeature { get; set; }
-
     }
 }
