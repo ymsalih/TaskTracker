d81ed8d89102bd6e3683b0f5c27b8c861f4c6aaa
Salih Atiç <147946955+ymsalih@users.noreply.github.com>
2025-07-28 11:58:45 +0300

    TaskUser verileri veritabanına eklendi.

---
+++ TaskTracker/Migrations/20250728063620_AddTaskUserRelation.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskUserRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaskUsers",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaskUsers", x => new { x.TaskId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_TaskUsers_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TaskUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskUsers_UserId",
+                table: "TaskUsers",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaskUsers");
+        }
+    }
+}
